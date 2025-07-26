07a9c48373b2b688c44f7d663caa68aa10e6ce0b
deepakachyutha <deepakbattula9160@gmail.com>
2025-07-26 22:23:12 +0530

    Adding my personal data and about me page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'IBM HR Analytics Employees Attrition & Performance',
+    description: `A data science project that predicts employee attrition using real-world HR data. Tackles imbalanced datasets using SMOTE and Class Weights, explains predictions with SHAP and Permutation Importance, and evaluates performance with Precision-Recall curves. Built with business level relevance and accuracy.`,
+    imgSrc: '/static/images/output.png',
+    href: 'https://github.com/deepakachyutha/IBMEmployees-ML',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Titanic-ML',
+    description: `This Titanic Survical Prediction project applies Supervised Machine Learning to predict passenger survival from the Titanic dataset. It focuses on practical data preprocessing, binary classification, and foundational model evaluation.`,
+    imgSrc: '/static/images/titanic.png',
+    href: 'https://github.com/deepakachyutha/Titanic-ML',
   },
 ]
 
