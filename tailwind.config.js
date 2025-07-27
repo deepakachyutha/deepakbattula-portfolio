7a4d3d946f8d00862f1080fde0059eccc3d75042
deepakachyutha <deepakbattula9160@gmail.com>
2025-07-27 20:42:30 +0530

    Colour and Font change

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,19 +26,7 @@ module.exports = {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
-        primary: {
-          50: '#eff6ff',
-          100: '#dbeafe',
-          200: '#bfdbfe',
-          300: '#93c5fd',
-          400: '#60a5fa',
-          500: '#0f0f0f', // Your color, now correctly in quotes
-          600: '#2563eb',
-          700: '#1d4ed8',
-          800: '#1e40af',
-          900: '#1e3a8a',
-          950: '#172554',
-        },
+        primary: colors.slate, // Use the "slate" palette for a great gray/black theme
         gray: colors.gray,
       },
       typography: ({ theme }) => ({
